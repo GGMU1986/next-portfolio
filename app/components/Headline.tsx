fadaffba781599cb23542c4edc3f7aa0e7b2a985
ggmu1986 <gt2186@gmail.com>
2024-03-18 14:42:02 -0400

    move navigator / up headline animation

--- app/components/Headline.tsx
+++ app/components/Headline.tsx
@@ -12,7 +12,7 @@ const Headline: React.FC<HeadlineProps> = ({ headline, className }) => {
   return (
     <motion.div 
       className={`animate-pulse font-semibold text-transparent bg-clip-text bg-gradient-to-r from-yellow-500 to-cyan-500 ${className}`}
-      initial={{ x: -600 }}
+      initial={{ x: -1200 }}
       animate={{ x: 0 }}
       transition={{ type: 'spring', stiffness: '120' }}
     >
--- app/components/Navigator.tsx
+++ app/components/Navigator.tsx
@@ -11,7 +11,7 @@ const Navigator = () => {
   const path = usePathname();
 
   return (
-    <div className='fixed flex flex-col bg-transparent gap-10 text-md sm:text-2xl right-4 top-[25%] sm:top-[45%] w-[3%] text-white justify-center items-center py-4 rounded-lg z-[11]'>
+    <div className='fixed flex flex-col bg-transparent gap-10 text-md sm:text-2xl left-4 top-[25%] sm:top-[12%] w-[3%] text-white justify-center items-center py-4 rounded-lg z-[11]'>
       {NAV_LINKS.map((nav, i) => {
         return (
           <Link key={i} href={nav.path}>

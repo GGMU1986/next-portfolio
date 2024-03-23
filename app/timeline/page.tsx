bdbc5352043fae2c88d715de88dcaca973821227
ggmu1986 <gt2186@gmail.com>
2024-03-23 12:34:45 -0400

    fix timeline mobile healine

--- app/timeline/page.tsx
+++ app/timeline/page.tsx
@@ -15,7 +15,7 @@ const Timeline = () => {
   // Map this after figuring out tailwind bug
   return (
     <div className='relative'>
-      <div className='absolute -top-[1.5rem] sm:top-[5rem] lg:top-[8rem] left-[10%] sm:left-[5%] lg:left-[10%] text-[1.5rem] xl:text-[2.5rem] 2xl:text-[3.1rem] flex gap-4 z-[9]'>
+      <div className='absolute top-[3.2rem] sm:top-[5rem] lg:top-[8rem] left-[10%] sm:left-[5%] lg:left-[10%] text-[1.5rem] xl:text-[2.5rem] 2xl:text-[3.1rem] flex gap-4 z-[9]'>
         <Headline headline='Experience' />
         <p className='font-bold text-white'>&amp;</p>
         <Headline headline='Education' />

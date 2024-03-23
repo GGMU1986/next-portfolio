f457706a1ec924f3f575add395479bb2c8f2834b
ggmu1986 <gt2186@gmail.com>
2024-03-23 12:20:30 -0400

    update navbar with more icons

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,10 +1,13 @@
 'use client';
 import React, { useEffect, useState } from 'react';
+import Link from 'next/link';
 import { motion } from 'framer-motion';
 import LinkImg from './LinkImg';
 import LinkText from './LinkText';
 import MobileNav from './MobileNav';
 import Navigator from './Navigator';
+
+import { IoDocumentText } from "react-icons/io5";
 
 const Navbar = () => {
   const [scrollPosition, setScrollPosition] = useState(0);
@@ -21,30 +24,40 @@ const Navbar = () => {
 
   return (
     <motion.div
-      className={`fixed top-0 z-[11] w-screen h-[3.05rem] md:h-[4.5rem] lg:h-[6.1rem] flex justify-between justify-center gap-2 items-center px-10 sm:px-20 ${ scrollPosition > 10 ? 'bg-slate-600 shadow' : 'bg-transparent' }`}
+      className={`fixed top-0 z-[11] w-screen h-[3.05rem] md:h-[4.5rem] lg:h-[6.1rem] flex justify-between justify-center items-center px-10 sm:px-20 ${ scrollPosition > 10 ? 'bg-slate-600 shadow' : 'bg-transparent' }`}
       initial={{ y: -100 }}
       animate={{ y: 0 }}
     >
-      <div className='flex items-center'>
+      <div className='flex items-center justify-between'>
         <MobileNav />
         <LinkImg
           href='/'
           src='/favicon.ico'
           alt='Logo'
-          className='w-[1.77rem] h-[1.77rem] md:w-[3.05rem] md:h-[3.05rem] lg:w-[4.2rem] lg:h-[4.2rem] rounded-full shadow-glow ml-2'
+          className='w-[1.77rem] h-[1.77rem] md:w-[3.05rem] md:h-[3.05rem] lg:w-[4.2rem] lg:h-[4.2rem] rounded-full shadow-glow ml-2 mr-10'
         />
-      </div>
-      <div className='flex gap-6 sm:gap-10 cursor-pointer text-white items-center'>
         <Navigator />
+      </div>
+      <div className='flex gap-5 sm:gap-10 cursor-pointer text-white items-center gap-small'>
+        <Link href='/navbar/George_Tsimis_Resume.pdf' target='blank' className='text-[1rem]'>
+          <IoDocumentText  className='resume-icon-show resume-icon-hide'/>
+        </Link>
         <LinkText
-          href='/George_Tsimis_Resume.pdf'
+          href='/navbar/George_Tsimis_Resume.pdf'
           text='RESUME'
-          className='font-semibold text-[0.74rem] md:text-[1rem] lg:text-[1.25rem] font-white tracking-[2px] hover:text-amber-300'
+          className='font-semibold text-[0.74rem] md:text-[1rem] lg:text-[1.25rem] font-white tracking-[2px] hover:text-amber-300 small-screen'
           target='_blank'
         />
         <LinkImg
+          href='https://github.com/GGMU1986'
+          src='/navbar/github.png'
+          alt='linkedin'
+          className='shadow-glow rounded-full w-[1.77rem] h-[1.77rem] md:w-[2.5rem] md:h-[2.5rem] lg:w-[3.1rem] lg:h-[3.1rem] hover:hoverGlow'
+          target='blank'
+        />
+        <LinkImg
           href='https://www.linkedin.com/in/george-tsimis-a5986224/'
-          src='/linkedin.png'
+          src='/navbar/linkedin.png'
           alt='linkedin'
           className='shadow-glow rounded-full w-[1.77rem] h-[1.77rem] md:w-[2.5rem] md:h-[2.5rem] lg:w-[3.1rem] lg:h-[3.1rem] hover:hoverGlow'
           target='blank'
--- app/components/Navigator.tsx
+++ app/components/Navigator.tsx
@@ -7,9 +7,9 @@ import { NAV_LINKS } from '@/constants/navLinks';
 
 const Navigator = () => {
   const path = usePathname();
-
+  
   return (
-    <div className={`hidden lg:flex lg:text-2xl md:gap-6 font-mono tracking-[.1rem] lg:gap-10 text-white justify-center items-center lg:ml-0 lg:mr-10 ${path === '/timeline' ? 'absolute flex-col left-4 top-[150%]' : ''}`}>
+    <div className='hidden lg:flex lg:text-2xl md:gap-6 font-mono tracking-[.1rem] lg:gap-10 text-white justify-center items-center lg:ml-0 lg:mr-10'>
       {NAV_LINKS.map((nav, i) => {
         return (
           <Link key={i} href={nav.path} className='nav relative'>

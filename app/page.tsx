1342dbfd0a55fe300372d767ce062eab6b1fb4c7
ggmu1986 <gt2186@gmail.com>
2024-03-26 09:32:47 -0400

    begin portrain border animation

--- app/page.tsx
+++ app/page.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 
 import TitleAndIntro from './components/TitleAndIntro';
 import Portrait from './components/Portrait';
+import PortraitV2 from './components/PortraitV2';
 
 export default function Home() {
   return (
     <div className='lg:flex lg:justify-between px-10 sm:px-20 w-full h-full bg-cover bg-center'>
       <TitleAndIntro />
-      <Portrait />
+      {/* <Portrait /> */}
+      <PortraitV2 />
     </div>
   );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,6 +8,21 @@ const config: Config = {
   ],
   theme: {
     extend: {
+      keyframes: {
+        'border-spin': {
+          '100%': {
+            transform: 'rotate(360deg)',
+          },
+        },
+        moveline: {
+          '0%': { transform: 'h-0' },
+          '100%': { transform: 'h-[100%]' },
+        },
+      },
+      animation: {
+        'border-spin': 'border-spin 7s linear infinite',
+        moveline: 'moveline 8s linear forwards',
+      },
       boxShadow: {
         glow: '0 0 20px theme(colors.white)',
       },

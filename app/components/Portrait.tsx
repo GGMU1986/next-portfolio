8da83720e1c1c0fa2a3e063d831dacf48601c607
ggmu1986 <gt2186@gmail.com>
2024-06-14 13:59:50 -0400

    update home page and pics

--- app/components/Portrait.tsx
+++ app/components/Portrait.tsx
@@ -14,27 +14,25 @@ const Portrait = () => {
     <motion.div
       initial={{ opacity: 0 }}
       animate={{ opacity: 1 }}
-      transition={{ duration: 5 }}
+      transition={{ duration: 3 }}
       className='card-wrapper w-[152px] h-[152px] sm:w-[254px] sm:h-[254px] lg:w-[308px] lg:h-[308px] xl:w-[410px] xl:h-[410px] bg-black rounded-xl cursor-pointer xl:mr-20 shadow-glow'
       onClick={() => setCurrent(current === length - 1 ? 0 : current + 1)}
     >
       {SELF_IMGS.map((self: SelfImage, i: number) => {
-        return (
-          <div
-            key={i}
-            className='mt-2 mr-8 w-[calc(100%-2px)] h-[calc(100%-2px)] absolute bottom-[17px] left-[1px] sm:bottom-[16px] sm:left-[2px] lg:bottom-[14px] lg:left-[4px] xl:bottom-[13px] xl:left-[5px]'
-          >
-            {i === current ? (
+        if (i === current) {
+          return (
+            <div
+              key={i}
+              className='mt-2 mr-8 w-[calc(100%-2px)] h-[calc(100%-2px)] absolute bottom-[17px] left-[1px] sm:bottom-[16px] sm:left-[2px] lg:bottom-[14px] lg:left-[4px] xl:bottom-[13px] xl:left-[5px]'
+            >
               <img
-                src={self.src}
-                alt={self.alt}
-                className='rounded-xl shadow-glow mt-4 w-[150px] h-[150px] sm:w-[250px] sm:h-[250px] lg:w-[300px] lg:h-[300px] xl:w-[400px] xl:h-[400px] object-cover'
-              />
-            ) : (
-              <></>
-            )}
-          </div>
-        );
+                  src={self.src}
+                  alt={self.alt}
+                  className='animate-image-change rounded-xl mt-4 w-[150px] h-[150px] sm:w-[250px] sm:h-[250px] lg:w-[300px] lg:h-[300px] xl:w-[400px] xl:h-[400px] object-cover'
+                />
+            </div>
+          );
+        }
       })}
     </motion.div>
   );

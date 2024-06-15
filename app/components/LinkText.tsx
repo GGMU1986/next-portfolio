116289850682cbb4e00ae917dee0aeae9079ac0f
ggmu1986 <gt2186@gmail.com>
2024-06-14 21:17:14 -0400

    refactor resume icon

--- app/components/LinkText.tsx
+++ app/components/LinkText.tsx
@@ -19,9 +19,9 @@ const LinkText: React.FC<LinkTextProps> = ({
 }) => {
   return (
     <Link href={href} target={target} className={className}>
-      <div className='group hover:translate-y-3 transition-all duration-500 flex flex-col items-center'>
-        <img className='cursor-pointer rounded-full w-[50px] h-[50px] lg:w-[50px] lg:h-[50px]' src='/navbar/resume.webp' alt='RESUME' />
-        <span className='absolute opacity-0 group-hover:bg-blue-900 p-2 text-sm rounded-lg text-center group-hover:text-white group-hover:opacity-100 group-hover:-translate-y-4 duration-700'>
+      <div className='group lg:hover:translate-y-3 transition-all duration-500 flex flex-col items-center'>
+        <img className='cursor-pointer rounded-full w-[25px] h-[25px] md:w-[33px] md:h-[33px] lg:w-[50px] lg:h-[50px]' src='/navbar/resume.webp' alt='RESUME' />
+        <span className='hidden lg:block absolute opacity-0 group-hover:bg-blue-900 p-2 text-sm rounded-lg text-center group-hover:text-white group-hover:opacity-100 group-hover:-translate-y-4 duration-700'>
           {text}
         </span>
       </div>
